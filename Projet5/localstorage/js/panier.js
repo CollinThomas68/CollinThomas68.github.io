43eebba7c7998eced7246ff385d2a2b92304ee7f
Thomas COLLIN <knutelator@hotmail.fr>
2020-06-29 18:19:28 +0200

    Bidouillage

---
+++ Projet5/localstorage/js/panier.js
@@ -0,0 +1,81 @@
+
+       if (JSON.parse(localStorage.getItem("panier") === "vide")) { //Panier vide 
+
+
+ 
+
+        
+        document.getElementById("resultatsPanier").innerHTML = 
+            '<div class="messagePanierVide">'+
+                '<p>Le panier est vide </p>'+
+                '<p><a href="orinoco.html"> Retour à l\'accueil </a></p>'+
+            '</div>';//panier vide dans index et Message
+/*       document.getElementById("contactPanier").innerHTML = 
+            '<article class="messagePanierVide">'+
+                '<a href="orinoco.html"><i class="fas fa-camera-retro"></i></a>'+
+            '</article>';//retour vers catalogue
+*/
+
+    } else { // panier NON vide
+        var produitsLocal = JSON.parse(localStorage.getItem("panier")); // Récupération du  panier
+        console.log(produitsLocal);
+
+        for (let x in produitsLocal) { //Inspection du panier
+            var ligneProduitLocal = produitsLocal[x]; //Recherche  ligneProduitLocal par nom
+            console.log(ligneProduitLocal);
+            // variation quantite - ou suppression a l'affichage
+            var signeMoinsPanier = "";
+            var plusOuMoins = String("moins");
+            if (ligneProduitLocal.quantite > 1) {
+                signeMoinsPanier = '<div class="sousElement-lignePanier"><button class="boutonPanierQuantite" onclick="fonctionQuantiteMoins(\'' + ligneProduitLocal.reference + '\')">-1</button></div>';
+            } else {
+                signeMoinsPanier = '<div class="sousElement-lignePanier"><button class="boutonPanierQuantite" onclick="fonctionDelete(' + x + ')">Suppression</i></button></div>';
+            }
+
+            // creationligne produit
+            let ligne =
+                '<article class="ContenairePanier">'+
+                    '<div class="contenaireLignePanier">'+
+                        '<div class="element-lignePanier"><img src="' + ligneProduitLocal.urlImage + '" title="' + ligneProduitLocal.reference + '"></div>'+
+                        '<div class="element-lignePanier">'+
+                            '<div class="element-optionPanier">' + ligneProduitLocal.nom + '</div>'+
+                            '<div class="element-optionPanier"><b>Option: </b>' + ligneProduitLocal.couleur + '</div>'+
+                        '</div>'+
+                        '<div class="element-lignePanier">' + ligneProduitLocal.description + '</div>'+
+                        '<div class="element-lignePanier"><b>Prix U:</b><br/>' + ligneProduitLocal.prixUnitaire / 100 + '€</div>'+
+                        '<div class="element-lignePanier"><b>Qté:</b><br/>' + ligneProduitLocal.quantite + '</div>'+
+                        '<div class="element-lignePanier"><b>Montant dû</b><br/>' + ligneProduitLocal.prixAjour / 100 + '€</div>'+
+                        '<div class="element-lignePanier">'+
+                        '<div class="sousElement-lignePanier"><button class="boutonPanierQuantite" onclick="fonctionQuantitePlus(\'' + ligneProduitLocal.reference + '\')">+1</button></div>' + signeMoinsPanier + '</div>'+
+                    '</div>'+
+                '</article>';
+
+
+
+            //affichage du panier sur le html
+            document.getElementById("resultatsPanier").innerHTML += ligne;
+        } // fin for produilocal
+        let partieContact =
+        '<article class="elementPanier">'+
+                    '<form name="formContact" id="idFormContact" onsubmit="return fonctionSubmitContact()">'+
+                        '<div class="element-form"><label>Nom*:</label></div>'+
+                            '<div class="element-form"><label><input type="text" name="nom" id="nom" class="decoInputContact" pattern="^[A-Z]+$" maxlenght="20" placeholder="Tout en MAJUSCULE !" required></label></div>'+
+                        '<div class="element-form"><label>Prénom*:</label></div>'+
+                            '<div class="element-form"><label><input type="text" name="prenom" id="prenom" pattern="^[A-ZÀÁÂÃÄÅÇÑñÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝ]{1}[a-zçàáâãäåçèéêëìíîïðòóôõöøùúûüýÿ]+$" maxlength="25" class="decoInputContact" placeholder="1 majuscule au début..." required></label></div>'+
+                        '<div class="element-form"><label>Adresse*:</label></div>'+
+                            '<div class="element-form"><label><input type="text" name="adresse" id="adresse" class="decoInputContact" placeholder="Numéro rue " maxlength="60" required></label></div>'+
+                        '<div class="element-form">CP*:</div>'+
+                            '<div class="element-form"><label><input type="text" name="codePostal" id="codePostal" class="decoInputContact" pattern="[0-9]{5}" maxlength="5" placeholder="5 chiffres !" required></label></div>'+
+                        '<div class="element-form"><label>Ville*:</label></div>'+
+                            '<div class="element-form"><label><input type="text" name="ville" id="ville" class="decoInputContact" pattern="^[A-ZÀÁÂÃÄÅÇÑñÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝ]+$" maxlength="30" placeholder="Tout en MAJUSCULE !"  required></label></div>'+
+                        '<div class="element-form"><label>Email*:</label></div>'+
+                            '<div class="element-form"><label><input type="email" name="mail" id="email" class="decoInputContact" placeholder="Une adresse email !"required></label></div>'+
+                        '<div id="inputContact">'+
+                            '<input type="hidden" name="actionPanier" value="eCommande">'+
+                            '<input type="submit" value="Valider la commande" id="bouton_envoi">'+
+                        '</div>'+
+                    '</form>'+
+                    '<p id="important">* Champ obligatoire</p>'+
+        '</article>';
+    document.getElementById("contactClient").innerHTML = partieContact;
+    }
---
+++ Projet5/localstorage/js/produit.js
@@ -0,0 +1,127 @@
+var teddyElt = document.getElementById("teddy");
+// Fonction permettant de récupérer l'id indiqué dans la barre d'adresse
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
+}
+var id = $_GET('id');
+
+
+
+
+ajaxGet(`http://localhost:3000/api/teddies/${id}`, function (reponse) {
+    // Récupère le contenu en fonction de l'id de la page
+    var teddy = JSON.parse(reponse);
+    var teddyCouleurs= teddy.colors;
+    /*console.log(teddy);
+    console.log(teddy.name);
+    console.log('Attention début du test');
+
+    console.log('Attention fin du test');
+    console.log(teddyCouleurs);
+    console.log(nbCouleurs);*/
+
+    var choixCouleurs="";
+    for (let x in teddyCouleurs) {
+        choixCouleurs += '<option value="' + teddyCouleurs[x] + '">' + teddyCouleurs[x] + '</option>';
+    }
+
+
+    var qteTeddy="";
+    for (q=1;q<10;q++){
+        qteTeddy+= '<option value="' + q + '">' + q + '</option>';
+    }
+
+
+
+
+    if(id==teddy._id){
+
+        let produitChoisi = '<article>' +
+                                '<div>'+
+                                    '<img src="'+teddy.imageUrl+'">'+
+                                '</div>'+
+                                '<div>'+
+                                    '<div>'+
+                                        '<h2>'+teddy.name+'</h2>'+
+                                        '<p>'+teddy.description+'</p>'+
+                                        '<p>'+teddy.price / 100+'€</p>'+
+                                        '<div>'+
+                                            '<form onsubmit="return fonctionAjoutProduit()" id="formulaireProduit">'+
+                                                '<label>Quantité : </label><select name="quantite" id="quantite">'+ qteTeddy+'</select>'+
+                                                '<label>Couleur : </label><select name="couleur" id="couleur">'+choixCouleurs+'</select>'+
+                                                '<input type="hidden" name="id" id="id" value="'+teddy._id+'">'+
+                                                '<input type="hidden" name="adresseHtml" id="adresseHtml" value="produit.html?id=' + teddy._id+'">'+
+                                                '<input type="hidden" name="name" id="name" value="'+teddy.name+'">'+
+                                                '<input type="hidden" name="description" id="description" value="'+teddy.description+'">'+
+                                                '<input type="hidden" name="prix" id="prix" value="'+teddy.price+'">'+
+                                                '<input type="hidden" name="urlImage" id="urlImage" value="'+teddy.imageUrl+'">'+
+                                                '<input type="submit" value="Ajout produit au panier" class="btnAjoutProduit"/>'+
+                                            '</form>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</article>';
+        
+        
+        let produitAffichage = document.getElementById('teddy');
+        produitAffichage.innerHTML= produitChoisi;
+    }
+});
+
+
+        /*
+        // Ajout du titre et du contenu de chaque article
+        var conteneurElt = document.createElement("article");
+        var lienElt = document.createElement("a");        
+        var imgElt = document.createElement('img');        
+        var nameElt = document.createElement("h2");
+        var descriptionElt = document.createElement("p");
+        var idElt = document.createElement("p");
+        var qteElt = document.createElement("div");
+        var couleurElt= document.createElement("div");
+        var couleurLabelElt=document.createElement("label");
+        var couleurSelectElt=document.createElement("select");
+       
+        conteneurElt.classList.add("col-sm-6");
+        imgElt.classList.add("portrait");
+        imgElt.src=teddy.imageUrl;
+        nameElt.textContent = teddy.name;
+        qteElt.innerHTML='<label for="q">Quantité: </label><select id="qt" name="q"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><button type="button" class="add-to-cart" data-id='+ teddy._id +' data-name='+ teddy.name +' data-price='+ teddy.price +' data-url="produit.html?id='+ teddy._id+'">Ajouter au panier</button>';
+        couleurSelectElt.classList.add("choixcouleurs");
+        teddyCouleurs.forEach(function(element){
+            var choixCouleurElt=document.createElement("option");
+            choixCouleurElt.innerHTML=element;
+            couleurSelectElt.appendChild(choixCouleurElt);
+        });
+        descriptionElt.textContent = teddy.description;
+        idElt.textContent ="Référence : " + teddy._id;
+        lienElt.href='produit.html?id=' + teddy._id;
+
+        teddyElt.appendChild(conteneurElt);
+        conteneurElt.appendChild(lienElt);           
+        lienElt.appendChild(imgElt);        
+        lienElt.appendChild(nameElt);
+        conteneurElt.appendChild(descriptionElt);
+        conteneurElt.appendChild(idElt);
+        conteneurElt.appendChild(qteElt);
+        conteneurElt.appendChild(couleurElt);
+        couleurElt.appendChild(couleurLabelElt);
+        couleurElt.appendChild(couleurSelectElt);
+
+
+    
+    }
+
+});
+        */
